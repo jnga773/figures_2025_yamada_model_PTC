9826d1a39d997ae440e101e4bf39f45128e1f6e1
Jacob Ngaha <jnga773@aucklanduni.ac.nz>
2025-03-03 19:33:07 +1300

    updated set_figure_dimensions.m

--- set_figure_dimensions.m
+++ set_figure_dimensions.m
@@ -1,4 +1,4 @@
-function set_figure_dimensions(width, height, padding, fontsize, units)
+function set_figure_dimensions(width, height, options)
   % set_figure_dimensions(width, height)
   %
   % Sets the dimensions of the axis borders to width_in x height_in. This
@@ -18,9 +18,11 @@ function set_figure_dimensions(width, height, padding, fontsize, units)
   %     Width of the axis plot. Default value is 6cm.
   % height : float
   %     Height of the axis plot. Default value is 4cm.
-  % Padding: float
+  % Padding : float
   %     Amount of padding around the axis Position. Defines the area of the
   %     OuterPosition property. Default value is 1cm.
+  % Scale : float
+  %     Scaling factor for figure dimensions. Default value is 1.
   % fontsize : float
   %     Fontsize of the figure labels and text etc. Default value is 9pt.
   % units : string
@@ -31,9 +33,12 @@ function set_figure_dimensions(width, height, padding, fontsize, units)
   arguments
     width    = 6.0;
     height   = 4.0;
-    padding  = 1.0;
-    fontsize = 9;
-    units    = 'centimeters';
+
+    % Optional arguments
+    options.padding  = 1.0;
+    options.scale    = 1.0;
+    options.fontsize = 9;
+    options.units    = 'centimeters';
   end
 
   % Get Matlab figure and axis properties
@@ -41,21 +46,24 @@ function set_figure_dimensions(width, height, padding, fontsize, units)
   ax  = gca();
 
   % Set units
-  fig.Units = units;
-  ax.Units  = units;
+  fig.Units = options.units;
+  ax.Units  = options.units;
 
   % Set fontsize
-  ax.FontSize = fontsize;
+  ax.FontSize = options.fontsize;
 
   % Set axis dimensions and position
-  ax.Position = [padding, padding, width, height];
+  ax.Position = [options.padding, options.padding, width, height];
 
   % Set figure dimensions and position, with extra padding
-  fig.Position = [5, 5, width + (2 * padding), height + (2 * padding)];
+  fig.Position = [5, 5, width + (2 * options.padding), height + (2 * options.padding)];
+
+  % Scale Positions
+  fig.Position = options.scale * fig.Position;
+  ax.Position  = options.scale * ax.Position;
   
   % Set paper size for printing
   fig.PaperPosition = [0, 0, fig.Position(3), fig.Position(4)];
   fig.PaperSize     = fig.Position(3:4);
-
 
 end
